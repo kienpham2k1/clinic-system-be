0151af3f1aa284851a47d901f62133b70722a0ab
KienPT <kienpt.dev.01@gmail.com>
2025-07-12 01:17:21 +0700

    spring swagger doc open api

---
+++ auth-service/src/main/java/org/example/authservice/config/WebCorsConfig.java
@@ -0,0 +1,18 @@
+package org.example.authservice.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/v3/api-docs")
+                .allowedOrigins("http://localhost:8080") // hoặc "*"
+                .allowedMethods("GET")
+                .allowedHeaders("*");
+    }
+}
---
+++ patient-service/src/main/java/org/example/patientservice/config/WebCorsConfig.java
@@ -0,0 +1,17 @@
+package org.example.patientservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/v3/api-docs")
+                .allowedOrigins("http://localhost:8080") // hoặc "*"
+                .allowedMethods("GET")
+                .allowedHeaders("*");
+    }
+}
