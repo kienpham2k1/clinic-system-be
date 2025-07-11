f52c21ba407177aed346fc1a5f3b0927042b7976
KienPT <kienpt.dev.01@gmail.com>
2025-07-11 08:44:14 +0700

    auth & gateway rate limiter

--- api-gateway/src/main/java/org/example/apigateway/config/RateLimiterConfig.java
+++ api-gateway/src/main/java/org/example/apigateway/config/RateLimiterConfig.java
@@ -1,12 +1,19 @@
 package org.example.apigateway.config;
 
+import io.jsonwebtoken.Claims;
+import org.example.commonservice.commonSecurity.utils.JwtPublicUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class RateLimiterConfig {
+    @Autowired
+    private JwtPublicUtil jwtPublicUtil;
+
     @Bean
     public KeyResolver ipKeyResolver() {
         return exchange -> Mono.just(
@@ -22,5 +29,17 @@ public class RateLimiterConfig {
     @Bean
     public KeyResolver userHeaderKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getHeaders().getFirst("X-User-Id"));
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> {
+            String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if (token != null && token.startsWith("Bearer ")) {
+                Claims claims = jwtPublicUtil.validateToken(token.substring(7));
+                return Mono.just(claims.get("username", String.class));
+            }
+            return Mono.just("anonymous");
+        };
     }
 }
