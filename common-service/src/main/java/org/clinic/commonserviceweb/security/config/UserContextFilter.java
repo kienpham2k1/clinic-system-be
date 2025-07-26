4be7f6302f26f2b3cb425028296e017aa44142d8
KienPT <kienpt.dev.01@gmail.com>
2025-07-26 14:10:34 +0700

    Permission / Role Checking

--- common-service/src/main/java/org/clinic/commonserviceweb/security/config/UserContextFilter.java
+++ common-service/src/main/java/org/clinic/commonserviceweb/security/config/UserContextFilter.java
@@ -1,27 +1,36 @@
 package org.clinic.commonserviceweb.security.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.clinic.commonserviceweb.exception.NotFoundException;
+import org.clinic.commonserviceweb.exception.AccessDeniedException;
+import org.clinic.commonserviceweb.exception.dto.error.ErrorResponseEntity;
 import org.clinic.commonserviceweb.security.dto.RequestPermissions;
 import org.clinic.commonserviceweb.security.dto.UserContext;
 import org.clinic.commonserviceweb.security.enums.Permission;
 import org.clinic.commonserviceweb.security.enums.Role;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.PathContainer;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.pattern.PathPattern;
 import org.springframework.web.util.pattern.PathPatternParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Component
 public class UserContextFilter extends OncePerRequestFilter {
     private static final PathPatternParser patternParser = new PathPatternParser();
 
@@ -41,6 +50,38 @@ public class UserContextFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        try {
+            securityChain(request, response, filterChain);
+            filterChain.doFilter(request, response);
+        } catch (AccessDeniedException ex) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("application/json");
+            response.getWriter().write(convertErrorToJson(ex, HttpStatus.FORBIDDEN, request.getRequestURI()));
+        } catch (Exception ex) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.setContentType("application/json");
+            response.getWriter().write(convertErrorToJson(ex, HttpStatus.INTERNAL_SERVER_ERROR, request.getRequestURI()));
+        } finally {
+            UserContextHolder.clear();
+        }
+    }
+
+    private String convertErrorToJson(Exception ex, HttpStatus httpStatus, String uri) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        ErrorResponseEntity errorResponse = ErrorResponseEntity.builder()
+                .status(httpStatus.value())
+                .error(httpStatus.getReasonPhrase())
+                .message(ex.getMessage())
+                .path(uri)
+                .timestamp(LocalDateTime.now())
+                .build();
+        return mapper.writeValueAsString(errorResponse);
+    }
+
+    protected void securityChain(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String path = request.getRequestURI();
         HttpMethod method = HttpMethod.valueOf(request.getMethod());
 
@@ -85,17 +126,11 @@ public class UserContextFilter extends OncePerRequestFilter {
                         accessDenied = permissions.stream().noneMatch(allowedPermissionSet::contains);
                     }
                     if (accessDenied) {
-                        throw new NotFoundException("Access denied: No permission config for path " + path);
+                        throw new AccessDeniedException("Access denied: No permission config for path " + path);
                     }
                 });
 
         UserContext context = new UserContext(userId, username, roles, permissions);
         UserContextHolder.setContext(context);
-
-        try {
-            filterChain.doFilter(request, response);
-        } finally {
-            UserContextHolder.clear();
-        }
     }
 }
